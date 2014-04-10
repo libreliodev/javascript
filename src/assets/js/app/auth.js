1691992c7f3836d0b0afa923a5c65c4e90b5fb07
Richard Ruiter <richard.ruiter@gmail.com>
2014-04-10 12:21:33 +0000

    #1 finished authentication

--- src/assets/js/app/auth.js
+++ src/assets/js/app/auth.js
@@ -4,45 +4,65 @@ $(function () {
     // define storages
     var ns = $.initNamespaceStorage('auth'),
         local = ns.localStorage,
-        session = ns.sessionStorage;
-    
+        session = ns.sessionStorage,
+		storage = (local.get('remember') === true) ? local : session,
+		authkeys = ['key', 'secret', 'dir', 'authvalid'],
+		submitkeys = ['submit_key','submit_secret','submit_dir'];
+
+	// callback for validation
+	var checkAuth = function() {
+		// check authentication
+		if (!storage.isSet(authkeys) && storage.get('authvalid') !== true) {
+		    location.href = 'login.html';
+		}
+	}
+
     // submitted?
-    var submitkeys = ['submit_key','submit_secret','submit_dir'];
     if (session.isSet(submitkeys)) {
+
+		// define parameters
+		var bucket = 'librelio-europe',
+			key = session.get('submit_key'),
+			secret = session.get('submit_secret'),
+			dir = session.get('submit_dir');
         
-        // check AWS
+        // setup AWS
         AWS.config.region = 'eu-west-1';
         AWS.config.update({
-            accessKeyId: session.get('submit_key'), 
-            secretAccessKey: session.get('submit_secret')
+            accessKeyId: key, 
+            secretAccessKey: secret
         });
-        var s3 = new AWS.S3({params: {Bucket: 'librelio-europe'}});
-        s3.listObjects(function(error,data) {
-             if (error === null) {
-                console.log(data);
-             } else {
-                console.log(error);
-             }
-          })
-        
+
+		// check auth + directory
+        var s3 = new AWS.S3({ region: AWS.config.region, maxRetries: 1 });
+        s3.listObjects({Bucket: bucket, Prefix: dir + "/", MaxKeys: 1}, function(error,data) {
+			data.Contents = data.Contents || [];
+			if (error === null && data.Contents.length > 0) {
+                storage.set('key', key);
+                storage.set('secret', secret);
+                storage.set('dir', dir);
+				storage.set('authvalid', true);
+			} else {
+				alert("Couldn't connect to aws s3: " + err);
+			}
+	
+			// check the authentication
+			checkAuth();
+		});
+
         // clear submitted data
-        //session.remove(submitkeys);
+        session.remove(submitkeys);
     }
-    
-    // check authentication
-    var storage = (local.get('remember') === true) ? local : session;
-    if (!storage.isSet(['key','secret','dir'])) {
-        //location.href = 'login.html';
-        
-    }
-    /*
-    var key = storage.get('key'),
-        secret = storage.get('secret'),
-        dir = storage.get('dir');
-        
-    if ()
-    */
-    
-    //ns.localStorage.set('remember', true);
+	else {
+		// check the authentication
+		checkAuth();
+	}
+
+	// logout button
+	$('.fa-power-off').click(function() {
+		$.removeAllStorages();
+		checkAuth();
+		return false;
+	});
 });
 
