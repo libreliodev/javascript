56187d8f4e5b74ded00a9d613ea3e1dd9df7854c
avinahum <aviavi8@gmail.com>
2014-04-19 20:47:16 +0300

    fix for #8 and #9

--- src/assets/js/app/publications.js
+++ src/assets/js/app/publications.js
@@ -1,7 +1,8 @@
-$(function(){
+$(function() {
 
     var appName = storage.getItem(config.storageAppNameKey);
-    var publicationsTable = $("#publicationDataTable");
+    var publicationsTable = $(".publicationDataTable").dataTable();
+    //publicationsTable.fnSort( [ [1,'asc'] ] );
 
     if (s3AuthObj && awsS3) {
 
@@ -12,24 +13,27 @@ $(function(){
             },
             function(error, apps) {
 
-                var appsList = apps.CommonPrefixes;
-
                 awsS3.getObject({
                     Bucket: config.s3Bucket,
                     Key: s3AuthObj.rootDirectory + '/'+appName+'/Magazines.plist'
                 }, function(err, activated) {
 
                     var appsList = apps.CommonPrefixes;
-                    var activeList = $.plist($.parseXML(activated.Body.toString()));
+                    var activeList =  PlistParser.parse($.parseXML(activated.Body.toString()));
+
+                    activeList = deleteFromObject(activeList, undefined);
+                    var activeListLength = $.map(activeList, function(n, i) { return i; }).length;
+
                     var rowsList = [];
                     var temp = {};
-/*console.log(appsList[2]);
-console.log(appsList[3]);
-console.log(appsList[4]);
-console.log(activeList[0]);
-console.log(activeList[1]);
-console.log(activeList[2]);*/
+                    var count = 0;
+
                     for(var i = 0; i < appsList.length; ++i) {
+
+                        //---------------------------------------------------
+                        // We have 3 unwanted folders...
+                        // ignore them and don't show them on the list
+                        //---------------------------------------------------
 
                         if (isolateFolderName(appsList[i].Prefix) == "AAD" ||
                             isolateFolderName(appsList[i].Prefix) == "APP__" ||
@@ -37,103 +41,226 @@ console.log(activeList[2]);*/
                             continue;
                         }
 
-                        temp[i] = {};
-                        temp[i].FolderName = isolateFolderName(appsList[i].Prefix);
-                        temp[i].id = i;
+                        //---------------------------------------------------
+                        // Prepear a single row object with default value
+                        //---------------------------------------------------
 
-                        for(var j = 0; j < activeList.length; ++j) {
-                            if (isolateFolderName2(activeList[j].FileName) == temp[i].FolderName) {
-                                temp[i].Title = activeList[j].Title;
-                                temp[i].Subtitle = activeList[j].Subtitle;
-                                temp[i].status = "active";
-                                temp[i].statusBtn = "<a class='btn  btn-danger btn-xs text-center btnInactive' href='#'>Inactive</a>";
-                            }
+                        temp[count] = {
+                            FolderName: isolateFolderName(appsList[i].Prefix),
+                            Title: "",
+                            Subtitle: "",
+                            status: "inactive",
+                            statusBtn: "<a data-filename='" + isolateFolderName(appsList[i].Prefix) + "' class='btn  btn-success btn-xs text-center btnActive' href='#'>Active</a>",
+                            id: 0
+                        }
 
-                            if (temp[i].Title == undefined) {
-                                temp[i].Title = "";
-                                temp[i].Subtitle = "";
-                                temp[i].status = "inactive";
-                                temp[i].statusBtn = "<a class='btn  btn-success btn-xs text-center btnActive' href='#'>Active</a>";
+                        for(var j = 0; j < activeListLength; ++j) {
+
+                            //---------------------------------------------------
+                            // There sometimes undefined keys is the object...
+                            // it cause because of inactive publications...
+                            // it's shouldn't happen, but we do taking
+                            // care of it, so the front end user wont have any
+                            // errors... jus in case...
+                            //---------------------------------------------------
+
+                            /*if (activeList[j] == undefined) {
+                             continue;
+                             }*/
+
+                            //---------------------------------------------------
+                            // Does the folder name fit each other?
+                            // if so update this publication title, subtitle
+                            // and status
+                            //---------------------------------------------------
+
+                            if (isolateFolderName2(activeList[j].FileName) == temp[count].FolderName) {
+                                temp[count].Title = activeList[j].Title;
+                                temp[count].Subtitle = activeList[j].Subtitle;
+                                temp[count].status = "active";
+                                temp[count].statusBtn = "<a data-filename='" + isolateFolderName(appsList[i].Prefix) + "' data-id='" + j + "' class='btn  btn-danger btn-xs text-center btnInactive' href='#'>Inactive</a>";
+                                temp[count].id = j;
+                                break;
                             }
                         }
 
-                        rowsList.push(temp[i]);
+                        rowsList.push(temp[count]);
+                        count++;
                     }
+
+                    //---------------------------------------------------
+                    // Add the rows to the table
+                    //---------------------------------------------------
 
                     for(var i = 0; i < rowsList.length; ++i) {
                         addRowToTable(rowsList[i], publicationsTable);
                     }
 
-                    activeDeactiveEvents();
+                    //---------------------------------------------------
+                    // Apply events for the active/inactive buttons
+                    //---------------------------------------------------
+
+                    activeInactiveEvents(publicationsTable);
                 });
             });
     }
 
 });
 
-function activeDeactiveEvents() {
+function activeInactiveEvents(publicationsTable) {
 
     $("a.btnActive").bind("click", function(e) {
         e.preventDefault();
-
-        var some_html = '<div class="form-group"> ' +
-                            '<label class="control-label col-lg-4">Title</label> ' +
-                            '<div class="col-lg-8"> ' +
-                                '<input type="text" /> ' +
-                            '</div> ' +
-                        '</div>';
-        some_html += '<div class="form-group"> ' +
-                        '<label class="control-label col-lg-4">Subtitle</label> ' +
-                        '<div class="col-lg-8"> ' +
-                        '<input type="text" /> ' +
-                        '</div> ' +
-                    '</div>';
-        bootbox.alert(some_html);
-
+        var obj = $(this);
+        activePublication(obj, publicationsTable);
     });
 
     $("a.btnInactive").bind("click", function(e) {
         e.preventDefault();
+        var obj = $(this);
+        inactivePublication(obj, publicationsTable);
+    });
+}
 
-        bootbox.dialog({
-            message: "Are you sure you want to inactive this publication?",
-            title: "Confirmation",
-            className: "littleModal",
-            buttons: {
-                success: {
-                    label: "Confirm",
-                    className: "btn-success",
-                    callback: function() {
-                        alert(1);
-                    }
-                },
-                danger: {
-                    label: "Cancel",
-                    className: "btn-danger",
-                    callback: function() {
-                        alert(0);
-                    }
+function activePublication(obj, publicationsTable) {
+    var some_html = '<form class="form-horizontal"> <div class="form-group"> ' +
+        '<label class="control-label col-lg-4">Title</label> ' +
+        '<div class="col-lg-8"> ' +
+        '<input type="text" name="pubTitleInput" /> ' +
+        '</div> ' +
+        '</div>';
+    some_html += '<div class="form-group"> ' +
+        '<label class="control-label col-lg-4">Subtitle</label> ' +
+        '<div class="col-lg-8"> ' +
+        '<input type="text" name="pubSubtitleInput" /> ' +
+        '</div> ' +
+        '</div></form>';
+
+    bootbox.dialog({
+        message: some_html,
+        title: "Confirmation",
+        className: "littleModal",
+        buttons: {
+            success: {
+                label: "Confirm",
+                className: "btn-success",
+                callback: function() {
+                    activeServerRequest(obj, publicationsTable);
                 }
+            },
+            danger: {
+                label: "Cancel",
+                className: "btn-danger",
+                callback: function() {}
+            }
+        }
+    });
+}
+
+function inactivePublication(obj, publicationsTable) {
+    bootbox.dialog({
+        message: "Are you sure you want to inactive this publication?",
+        title: "Confirmation",
+        className: "littleModal",
+        buttons: {
+            success: {
+                label: "Confirm",
+                className: "btn-success",
+                callback: function() {
+                    inactiveServerRequest(obj, publicationsTable);
+                }
+            },
+            danger: {
+                label: "Cancel",
+                className: "btn-danger",
+                callback: function() {}
+            }
+        }
+    });
+}
+
+function activeServerRequest(obj, publicationsTable) {
+    var appName = storage.getItem(config.storageAppNameKey);
+
+    var pTitle = $("input[name='pubTitleInput']").val();
+    var pSubtitle = $("input[name='pubSubtitleInput']").val();
+
+    awsS3.getObject({
+        Bucket: window.config.s3Bucket,
+        Key: window.s3AuthObj.rootDirectory + '/'+appName+'/Magazines.plist'
+    }, function(err, activated) {
+
+        var activeList = PlistParser.parse($.parseXML(activated.Body.toString()));
+        var activeListLength = $.map(activeList, function(n, i) { return i; }).length;
+
+        activeList[activeListLength] = {
+            FileName: obj.data("filename") + "/" + obj.data("filename") + "_.pdf",
+            Title: pTitle,
+            Subtitle: pSubtitle
+        };
+
+        var params = {
+            Bucket: window.config.s3Bucket, // required
+            Key: window.s3AuthObj.rootDirectory + '/'+appName+'/Magazines.plist',
+            Body: PlistParser.toPlist(activeList)
+        };
+        window.awsS3.putObject(params, function(err, data) {
+            if (err) {
+                alert(Error);
+            } else {
+                //obj.addClass("btnInactive").addClass("btn-danger").removeClass("btnActive").removeClass("btn-success").html("Inactive").data("id", activeListLength);
+                //publicationsTable.fnGetPosition( obj.parents('tr').closest('.ttitle')[0]).html(pTitle);
+                //publicationsTable.fnGetPosition( obj.parents('tr').closest('.tsubtitle')[0]).html(pSubtitle);
+                location.reload();
             }
         });
 
     });
 }
 
-function activePublication() {
+function inactiveServerRequest(obj, publicationsTable) {
+    var appName = storage.getItem(config.storageAppNameKey);
 
-}
+    awsS3.getObject({
+        Bucket: window.config.s3Bucket,
+        Key: window.s3AuthObj.rootDirectory + '/'+appName+'/Magazines.plist'
+    }, function(err, activated) {
 
-function deactivePublication() {
+        var activeList = PlistParser.parse($.parseXML(activated.Body.toString()));
 
+        var id = obj.data("id");
+
+        delete activeList[id];
+
+        activeList = deleteFromObject(activeList, undefined);
+
+        //var rowIndex = publicationsTable.fnGetPosition( obj.closest('tr')[0] );
+        //publicationsTable.fnDeleteRow(rowIndex);
+
+        var params = {
+            Bucket: window.config.s3Bucket, // required
+            Key: window.s3AuthObj.rootDirectory + '/'+appName+'/Magazines.plist',
+            Body: PlistParser.toPlist(activeList)
+        };
+        window.awsS3.putObject(params, function(err, data) {
+            if (err) {
+                alert(Error);
+            } else {
+                //obj.removeClass("btnInactive").removeClass("btn-danger").addClass("btnActive").addClass("btn-success").html("Active").data("id", 0);
+                //publicationsTable.fnGetPosition( obj.parents('tr').closest('.ttitle')[0]).html("");
+                //publicationsTable.fnGetPosition( obj.parents('tr').closest('.tsubtitle')[0]).html("");
+                location.reload();
+            }
+        });
+
+    });
 }
 
 function addRowToTable(data, publicationsTable) {
-
-    publicationsTable.dataTable().fnAddData( [
+    publicationsTable.fnAddData( [
         data.FolderName,
-        data.Title,
-        data.Subtitle,
+        "<span class='ttitle'>" + data.Title + "</span>",
+        "<span class='tsubtitle'>" + data.Subtitle + "</span>",
         "<a class='btn  btn-primary btn-xs text-center' href='#'>Edit</a>",
         data.statusBtn]
     );
@@ -145,4 +272,53 @@ function isolateFolderName(name) {
 
 function isolateFolderName2(name) {
     return name.substring(	name.indexOf("/")+1, name.length-5);
+}
+
+function deleteFromObject(obj, deleteValue) {
+    var objToArray = $.map(obj, function(value, index) {
+        return [value];
+    });
+
+    for (var i = 0; i < objToArray.length; ++i) {
+        if (objToArray[i] == deleteValue) {
+            objToArray.splice(i, 1);
+            i--;
+        }
+    }
+
+    return ArrayToObject(objToArray);
+}
+
+function ArrayToObject(arr) {
+    var rv = {};
+    for (var i = 0; i < arr.length; ++i)
+        rv[i] = arr[i];
+    return rv;
+}
+
+function del(keytr) {
+    var appName = storage.getItem(config.storageAppNameKey);
+
+    awsS3.getObject({
+        Bucket: window.config.s3Bucket,
+        Key: window.s3AuthObj.rootDirectory + '/'+appName+'/Magazines.plist'
+    }, function(err, activated) {
+
+        var activeList = PlistParser.parse($.parseXML(activated.Body.toString()));
+
+        delete activeList[keytr];
+
+        activeList = deleteFromObject(activeList, undefined);
+
+
+        var params = {
+            Bucket: window.config.s3Bucket, // required
+            Key: window.s3AuthObj.rootDirectory + '/'+appName+'/Magazines.plist',
+            Body: PlistParser.toPlist(activeList)
+        };
+        window.awsS3.putObject(params, function(err, data) {
+
+        });
+
+    });
 }
