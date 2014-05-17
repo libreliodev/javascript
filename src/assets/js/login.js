e2ff19fba50b7a0e3be6f03a6feb51efcba87ade
Saeed <info@serverfire.net>
2014-05-17 12:35:15 +0430

    #26 add login method c1(unfinished)

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -42,9 +42,7 @@ $(function(){
                        if(selectedApp)
                            storage.setItem(config.storageAppNameKey,
                                            selectedApp);
-                       var dq = path.urlParseQuery(document.location);
-                       document.location = dq.redirect ? 
-                           dq.redirect : 'index.html';
+                       redirectLoggedInUser();
                    }
                    if(err)
                    {
@@ -62,6 +60,7 @@ $(function(){
                        else
                        {
                            var auth_obj = {
+                               method: 'main',
                                accessKeyId: accessKeyId,
                                secretAccessKey: secretAccessKey,
                                rootDirectory: rootDirectory
@@ -73,12 +72,8 @@ $(function(){
                            
                            storage.type = storage_t;
                            var prevObj = storage.getItem(config.storageAuthKey);
-                           if(!prevObj || prevObj.accessKeyId != accessKeyId)
-                           {
-                               // clear user info
-                               storage.setItem(config.storageAppNameKey, '');
-                               storage.setItem(config.singleAppModeKey, '');
-                           }
+                           if(!prevObj || prevObj.accessKeyId != accessKeyId) 
+                               clearUserStorage(); // clear user info
                            storage.setItem(config.storageAuthKey, JSON.stringify(auth_obj));
                            if(selectedApp)
                            {
@@ -109,7 +104,136 @@ $(function(){
                    $submitBtn.ladda( 'stop' );
                    $('button', form).prop('disabled', false);
                });
-            $submitBtn.ladda( 'stop' );
             return false;
         });
 });
+function idFedLogin(opts, cb)
+{
+    /* opts_ex = {
+         cred: {
+           RoleArn: roleArn, WebIdentityToken: token, ProviderId: providerId
+         },
+         userId: userId,
+         host: host // like 'google.com' || 'facebook.com'
+       }
+     */
+    function testPermission(cb)
+    {
+        // caller should userId in opts.userId;
+        // requests for users directory
+        // at end of this method life it will call `cb(err)' and if there is
+        var testfile = userDir + '/tf';
+        s3.putObject({
+            Bucket: config.s3Bucket,
+            Key: testfile
+        }, function()
+           {
+               if(err)
+                   return cb && cb(err);
+               s3.deleteObject({
+                   Bucket: config.s3Bucket,
+                   Key: testfile
+               }, function(err, data)
+                  {
+                      cb && cb(err);
+                  });
+           });
+    }
+    AWS.config.cerdentials = new AWS.WebIdentityCredentials(opts.cred);
+
+    AWS.config.region = config.s3BucketRegion;
+    var s3 = new AWS.S3({ region: config.s3BucketRegion, maxRetries: 1 }),
+    rootDir = config.idFedS3RootDirectory,
+    userDir = rootDir + '/' + opts.userId;
+    $('button', form).prop('disabled', true);
+    s3.listObjects({
+        Bucket: config.s3Bucket,
+        Prefix: userDir,
+        MaxKeys: 1
+    }, function(err, data)
+       {
+           if(err)
+           {
+               alert("Couldn't connect to aws s3: " + err);
+           }
+           else if(!data || !data.Contents || data.Contents.length <= 0)
+           {
+               testPermission(function(err)
+                 {
+                     if(err)
+                         return cb && cb(err);
+                     continue_job();
+                 });
+           }
+           else
+               continue_job();
+       });
+    function continue_job()
+    {
+        var auth_obj = {
+            type: 'idFed',
+            host: opts.host,
+            cred: opts.cred,
+            userId: opts.userId,
+            rootDirectory: rootDir
+        };
+        storage.type = 'local';
+        storage.setItem('storage-type', storage_t);
+        
+        var prevObj = storage.getItem(config.storageAuthKey);
+        if(!prevObj || prevObj.host != auth_obj.host || 
+           prevObj.userId != auth_obj.userId)
+            clearUserStorage(); // clear user info
+        storage.setItem(config.storageAuthKey, JSON.stringify(auth_obj));
+        storage.setItem(config.singleAppModeKey, "1");
+        storage.setItem(config.storageAppNameKey, opts.userId);
+        redirectLoggedInUser();
+        cb && cb();
+    }
+}
+function alertIfError(err)
+{
+    if(err)
+        alert(err);
+}
+function loggedInFacebook(response)
+{
+    if(response.status == 'connected')
+    {
+        var authResp = response.authResponse;
+        idFedLogin({
+            userId: authResp.userID,
+            host: 'facebook.com',
+            cred: {
+                RoleArn: config.idFedFBUsersRoleArn,
+                WebIdentityToken: authResp.accessToken,
+                ProviderId: 'graph.facebook.com'
+            }
+        }, alertIfError);
+    }
+}
+function loggedInGooglePlus(response)
+{
+    console.log(arguments);
+    if(!response.error && 0)
+    {
+        idFedLogin({
+            userId: authResp.userID,
+            host: 'google.com',
+            cred: {
+                RoleArn: config.idFedUsersRoleArn,
+                WebIdentityToken: response.id_token
+            }
+        }, alertIfError);
+    }
+}
+function redirectLoggedInUser()
+{
+    var dq = path.urlParseQuery(document.location);
+    document.location = dq.redirect ? dq.redirect : 'index.html';
+}
+function clearUserStorage()
+{
+    storage.setItem(config.storageAppNameKey, '');
+    storage.setItem(config.singleAppModeKey, '');
+}
