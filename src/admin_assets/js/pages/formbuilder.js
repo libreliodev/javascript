b5efaa126160895589819640cc316d4c08afb06c
Saeed <info@serverfire.net>
2014-07-15 17:12:44 +0430

    load/save/open in spreadsheet and formbuilder #40 & #31

---
+++ src/admin_assets/js/pages/formbuilder.js
@@ -0,0 +1,96 @@
+var doc_query = querystring.parse(get_url_query(document.location+'')),
+fb_key = doc_query ? doc_query.key : null;
+$(function(){
+  var formbuilder,
+  save_request,
+  app_name = storage.getItem(config.storageAppNameKey),
+  app_dir = get_app_dir(app_name);
+
+  
+  if(fb_key)
+  {
+    s3_load_json_data(fb_key, function(err, obj)
+      {
+        if(err && err.code != 'NoSuchKey')
+          notifyUserError(err);
+        init_formbuilder(obj || {
+          fields: []
+        });
+      });
+  }
+  
+  function init_formbuilder(data)
+  {
+    formbuilder = new Formbuilder({
+      selector: '.fb-main',
+      bootstrapData: data.fields
+    });
+    formbuilder.on('save', function(payload)
+      {
+        save_data(payload);
+      })
+  }
+
+  $('#open-btn').click(function()
+    {
+      openPublicationDialog({
+        app_dir: app_dir,
+        extension: '.fb.json'
+      }, function(err, res)
+         {
+           if(err)
+             return notifyUserError(err);
+           if(res)
+             document.location = 'formbuilder.html?key=' + res.key;
+         });
+    });
+  function save_data(obj, cb)
+  {
+    if(save_request)
+      save_request.abort();
+    save_request = s3_save_json_data(fb_key, obj, function(err)
+      {
+        save_request = null;
+        if(err)
+        {
+          if(cb)
+            cb(err);
+          else
+            notifyUserError(err);
+          return;
+        }
+        cb && cb();
+      });
+  }
+
+});
+function s3_load_json_data(key, cb)
+{
+  awsS3.getObject({
+    Bucket: config.s3Bucket,
+    Key: key
+  }, function(err, res)
+     {
+       if(err)
+         cb && cb(err);
+       else
+         try {
+           cb && cb(undefined, JSON.parse(res.Body.toString()));
+         } catch(e) {
+           cb && cb(new Error(
+             sprintf("Couldn't parse formbuilder file in %s", key)));
+         }
+     });
+}
+function s3_save_json_data(key, obj, cb)
+{
+  return awsS3.putObject({
+    Bucket: config.s3Bucket,
+    Key: key,
+    Body: typeof obj == 'string' ? obj : JSON.stringify(obj),
+    ContentType: 'application/json'
+  }, function(err)
+     {
+       cb && cb(err);
+     });
+}
--- src/admin_assets/js/pages/spreadsheet.js
+++ src/admin_assets/js/pages/spreadsheet.js
@@ -1,61 +1,81 @@
+var doc_query = querystring.parse(get_url_query(document.location+'')),
+csv_key = doc_query ? doc_query.key : null;
 $(function(){
   var autosaveNotification,
   save_request,
   app_name = storage.getItem(config.storageAppNameKey),
   app_dir = get_app_dir(app_name),
   $console = $('#status-msg'),
-  startCols = 8, minSpareRows = 1,
-  $spreadsheet = $("#spreadsheet").handsontable({
-    startCols: startCols,
-    rowHeaders: true,
-    colHeaders: true,
-    minSpareRows: minSpareRows,
-    contextMenu: true,
-    afterChange: function(change, source)
-    {
-      if (source === 'loadData')
-        return; //don't save this change
-      if($('#autosave_b').is(':checked'))
-      {
-        clearTimeout(autosaveNotification);
-        if(save_request)
-          save_request.abort();
-        save_request = spreadsheet_save_on_s3(app_dir + 
-                                                  '/sample-spreadsheet.json', 
-                                                  spreadsheet.getData(), 
-                                                  function(err)
-          {
-            save_request = null;
-            if(err)
-              $console.text(err);
-            else
-            {
-              $console.text('Autosaved (' + change.length + ' ' +
-                            'cell' + (change.length > 1 ? 's' : '') + ')');
-              autosaveNotification = setTimeout(function()
-                {
-                  $console.text('Changes will be autosaved');
-                }, 1000);
-            }
-          });
-      }
-    }
-  }),
-  spreadsheet = $spreadsheet.data('handsontable');
+  startCols = 8, minSpareRows = 1, $spreadsheet, spreadsheet;
 
-  load_data();
+  if(csv_key)
+  {
+    init_spreadsheet();
+    load_data(csv_key);
+  }
+  function init_spreadsheet()
+  {
+    $spreadsheet = $("#spreadsheet").handsontable({
+      startCols: startCols,
+      rowHeaders: true,
+      colHeaders: true,
+      minSpareRows: minSpareRows,
+      contextMenu: true,
+      afterChange: function(change, source)
+      {
+        if (source === 'loadData')
+          return; //don't save this change
+        if($('#autosave_b').is(':checked'))
+        {
+          clearTimeout(autosaveNotification);
+          if(save_request)
+            save_request.abort();
+          save_request = spreadsheet_save_on_s3(csv_key, spreadsheet.getData(), 
+                                                function(err)
+            {
+              save_request = null;
+              if(err)
+                $console.text(err);
+              else
+              {
+                $console.text('Autosaved (' + change.length + ' ' +
+                              'cell' + (change.length > 1 ? 's' : '') + ')');
+                autosaveNotification = setTimeout(function()
+                  {
+                    $console.text('Changes will be autosaved');
+                  }, 1000);
+              }
+            });
+        }
+      }
+    });
+    spreadsheet = $spreadsheet.data('handsontable');
+  }
+
   $('#clear-btn').click(function()
     {
       clear_data();
-      return false;
+    });
+  $('#open-btn').click(function()
+    {
+      openPublicationDialog({
+        app_dir: app_dir,
+        extension: '.csv'
+      }, function(err, res)
+         {
+           if(err)
+             return notifyUserError(err);
+           if(res)
+             document.location = 'spreadsheet.html?key=' + res.key;
+         });
     });
   $('#save-btn').click(function()
     {
+      if(!csv_key)
+        return;
       if(save_request)
         save_request.abort();
-      save_request = spreadsheet_save_on_s3(app_dir + 
-                                            '/sample-spreadsheet.json', 
-                                            spreadsheet.getData(), 
+      save_request = spreadsheet_save_on_s3(csv_key, spreadsheet.getData(), 
                                             function(err)
         {
           save_request = null;
@@ -67,23 +87,25 @@ $(function(){
     });
   $('#load-btn').click(function()
     {
-      load_data();
+      load_data(csv_key);
     });
-  function load_data(cb)
+  function load_data(key, cb)
   {
     $console.text('Loading...');
-    spreadsheet_load_from_s3(app_dir + '/sample-spreadsheet.json', 
-                             function(err, res)
+    spreadsheet_load_from_s3(key, function(err, res)
       {
         if(err && err.code != 'NoSuchKey')
+        {
           $console.text(err);
+          return cb && cb(err);
+        }
         else if(res)
         {
           try {
             spreadsheet.loadData(res);
           } catch(e) {
             err = _("Couldn't read spreadsheet data");
-            $console.text(text);
+            $console.text(err);
           }
         }
         else
@@ -106,11 +128,11 @@ $(function(){
   }
   function spreadsheet_save_on_s3(key, data, cb)
   {
+    var csv_str = d3.csv.format(data);
     return awsS3.putObject({
       Bucket: config.s3Bucket,
       Key: key,
-      Body: JSON.stringify(data),
-      ContentType: 'application/json'
+      Body: csv_str
     }, function(err, res)
        {
          cb && cb(err);
@@ -126,7 +148,7 @@ $(function(){
          if(err)
            return cb && cb(err);
          try {
-           cb(undefined, JSON.parse(res.Body.toString()));
+           cb(undefined, d3.csv.parseRows(res.Body.toString()));
          }catch(e) {
            cb && cb(new Error(
              sprintf(_("Couldn't parse spreadsheet file in %s"), key)));
