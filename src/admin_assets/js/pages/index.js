143e0a1893978ea08aa9824df2fbf39cfca36c03
Saeed <info@serverfire.net>
2014-10-21 17:48:45 +0330

    #138 fix

--- src/admin_assets/js/pages/index.js
+++ src/admin_assets/js/pages/index.js
@@ -4,30 +4,9 @@ $(function()
   app_dir = get_app_dir(app_name);
   if(!app_name)
     return;
-  awsS3Ready(function()
+  awsCredentialsReady(function()
     {
       $('#publisher-name2').text(s3AuthObj.rootDirectory);
 
-      s3ObjectExists(awsS3, {
-        Bucket: config.s3Bucket,
-        Key: app_dir + '/APP_/Uploads/setup.plist',
-      }, function(err, exist)
-         {
-           if(err)
-             return handleAWSS3Error(err);
-
-           if(!exist)
-             return notifyUserError("You should setup app first! <a href=\"setup.html\">Click Here!</a>");
-           var reader = $('<iframe/>');
-           reader.prop('id', 'reader');
-           reader.attr('src', 'http://reader.librelio.com?' + 
-                                             querystring.stringify({
-                                               wapublisher: 
-                                                     s3AuthObj.rootDirectory,
-                                               waapp: app_name, 
-                                               waversion: 'html5'
-                                             }));
-           $('#reader-wrapper').append(reader);
-         });
     });
 });
---
+++ src/admin_assets/js/pages/preview.js
@@ -0,0 +1,37 @@
+$(function()
+{
+  var app_name = storage.getItem(config.storageAppNameKey),
+  app_dir = get_app_dir(app_name);
+  if(!app_name)
+    return;
+  awsS3Ready(function()
+    {
+      s3ObjectExists(awsS3, {
+        Bucket: config.s3Bucket,
+        Key: app_dir + '/APP_/Uploads/setup.plist',
+      }, function(err, exist)
+         {
+           if(err)
+             return handleAWSS3Error(err);
+
+           if(!exist)
+             return notifyUserError("You should setup app first! <a href=\"setup.html\">Click Here!</a>");
+           $('#reader-link').attr('href', 'http://reader.librelio.com?' + 
+                                             querystring.stringify({
+                                               wapublisher: 
+                                                     s3AuthObj.rootDirectory,
+                                               waapp: app_name
+                                             }));
+           var reader = $('<iframe/>');
+           reader.prop('id', 'reader');
+           reader.attr('src', 'http://reader.librelio.com?' + 
+                                             querystring.stringify({
+                                               wapublisher: 
+                                                     s3AuthObj.rootDirectory,
+                                               waapp: app_name, 
+                                               waversion: 'html5'
+                                             }));
+           $('#reader-wrapper').append(reader);
+         });
+    });
+});
